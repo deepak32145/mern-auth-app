13b4cdca83850839216bab5e1e13c378e8ada0a1
Deepak Choudhary <deepak32145@gmail.com>
2024-08-09 14:41:42 +0530

    added model

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,16 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup Route");
-});
-router.get("/login", (req, res) => {
-  res.send("login Route");
-});
-
-router.get("/logout", (req, res) => {
-  res.send("logout Route");
-});
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 
 export default router;
