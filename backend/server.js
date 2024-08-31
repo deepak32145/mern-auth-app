f9cced3a6a734a07877dd1210b34458dd670c592
Deepak Choudhary <deepak32145@gmail.com>
2024-08-31 14:48:44 +0530

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./db/connectDb.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -9,8 +10,10 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,8 +23,14 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("server is running on port 3000");
 });
